908d6472710cce25ec38a65c7ae22dde2bd951bd
Vijaya Kumar V <vijayvulchi@users.noreply.github.com>
2019-05-11 19:28:47 +0530

    validations and error messages

--- backend/app.js
+++ backend/app.js
@@ -14,6 +14,14 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use('/api', rtsIndex);
 
+// error handler
+app.use((err, reg, res, next) => {
+	if (err.name === 'ValidationError') {
+		var valErrors = [];
+		Object.keys(err.errors).forEach(key => valErrors.push(err.errors[key].message));
+		res.status(422).send(valErrors);
+	}
+});
 
 // start server
 app.listen(process.env.PORT, () => console.log(`Server started at port : ${process.env.PORT}`));
