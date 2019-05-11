02b7fde44122dea989165de3e21c424f3bd9ca48
Vijaya Kumar V <vijayvulchi@users.noreply.github.com>
2019-05-11 16:38:33 +0530

    initial commit backend

---
+++ backend/app.js
@@ -0,0 +1,19 @@
+require('./config/config');
+require('./models/db');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const rtsIndex = require('./routes/index');
+
+var app = express();
+
+// middleware
+app.use(bodyParser.json());
+app.use(cors());
+app.use('/api', rtsIndex);
+
+
+// start server
+app.listen(process.env.PORT, () => console.log(`Server started at port : ${process.env.PORT}`));
